61b12aa0a33d15ab83df997e4c2f291719af4556
Shreyansh Pandey <shreyansh7234@gmail.com>
2026-07-24 17:13:26 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.65%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0||x==1)return x;
+        int l=1;
+        int h=x;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if((long)mid*mid>x){
+                h=mid-1;
+            }
+            else if(mid*mid==x)return mid;
+            else{
+                l=mid+1;
+            }
+        }
+        return static_cast<int>(std::round(h));
+    }
+};
