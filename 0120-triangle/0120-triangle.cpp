ed6f4e1c311a9fba6554141fde9f542008d532c9
Shreyansh Pandey <shreyansh7234@gmail.com>
2026-07-28 21:42:47 +0530

    Time: 4 ms (6.2%), Space: 13.5 MB (9.07%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minway(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp){
+        int n=triangle.size();
+        
+        if(i==n-1){
+            return triangle[i][j];
+        }
+        if(dp[i][j]!=INT_MAX){
+            return dp[i][j];
+        }
+        int left=minway(i+1,j,triangle,dp);
+        int right=minway(i+1,j+1,triangle,dp);
+        return dp[i][j]=triangle[i][j]+min(left,right);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        int m=triangle[0].size();
+        vector<vector<int>>dp(n,vector<int>(n,INT_MAX));
+        return minway(0,0,triangle,dp);
+    }
+};
