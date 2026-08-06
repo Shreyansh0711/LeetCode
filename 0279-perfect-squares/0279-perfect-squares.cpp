78303054f6980ae593eeebaff25b995233df41e4
Shreyansh Pandey <shreyansh7234@gmail.com>
2026-08-06 16:54:34 +0530

    Time: 39 ms (83.29%), Space: 13.3 MB (56.94%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
-    int find(int idx, int tar, vector<int>& nums, vector<vector<int>>& dp) {
-        if (idx == 0) {
-            if (tar % nums[idx] == 0)
-                return tar / nums[idx];
-            else
-                return INT_MAX;
-        }
-        if (dp[idx][tar] != -1)
-            return dp[idx][tar];
-        int ntake = find(idx - 1, tar, nums, dp);
-        int take = INT_MAX;
-        if (nums[idx] <= tar) {
-            int res = find(idx, tar - nums[idx], nums, dp);
-            if (res != INT_MAX)
-                take = 1 + res;
-        }
-        return dp[idx][tar] = min(take, ntake);
-    }
     int numSquares(int n) {
-        vector<int> nums;
-        for (int i = 1; i * i <= n; i++) {
-            nums.push_back(i * i);
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = min(dp[i], 1 + dp[i - j * j]);
+            }
         }
-        vector<vector<int>> dp(nums.size(), vector<int>(n + 1, -1));
-        return find(nums.size() - 1, n, nums, dp);
+
+        return dp[n];
     }
 };
