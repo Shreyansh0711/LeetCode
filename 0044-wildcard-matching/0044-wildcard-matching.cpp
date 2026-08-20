54d6c13e8723ac1092799618f41a184ce33d85ff
Shreyansh Pandey <shreyansh7234@gmail.com>
2026-08-20 16:39:06 +0530

    Time: 76 ms (18.79%), Space: 54.6 MB (18.78%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -29,8 +29,23 @@ public:
         int n = s.size();
         int m = p.size();
 
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-
-        return find(n - 1, m - 1, s, p, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+        dp[0][0] = 1;
+        for(int j=1;j<=m;j++){
+            if(p[j-1]=='*')dp[0][j]=dp[0][j-1];
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if (p[j-1] =='*') {
+                    dp[i][j] =
+                        dp[i - 1][j]|
+                        dp[i][j - 1];
+                }
+                if (s[i-1] == p[j-1] || p[j-1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
